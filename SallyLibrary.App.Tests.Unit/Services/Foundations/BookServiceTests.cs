737c7faf7604f913996210660c2ac67676fcf176
Hassan Habib <harezk@microsoft.com>
2021-09-24 20:40:47 -0700

    ShouldAddBook -> FAIL

---
+++ SallyLibrary.App.Tests.Unit/Services/Foundations/BookServiceTests.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using SallyLibrary.App.Brokers.Storages;
+using SallyLibrary.App.Models.Books;
+using SallyLibrary.App.Services.Foundations.Books;
+using Xunit;
+
+namespace SallyLibrary.App.Tests.Unit.Services.Foundations
+{
+    public class BookServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IBookService bookService;
+
+        public BookServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+
+            this.bookService = new BookService(
+                storageBroker: this.storageBrokerMock.Object);
+        }
+
+        [Fact]
+        public void ShouldAddBook()
+        {
+            // given
+            var randomBook = new Book();
+            Book inputBook = randomBook;
+            Book storageBook = inputBook;
+            Book expectedBook = storageBook;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertBook(inputBook))
+                    .Returns(storageBook);
+
+            // when
+            Book actualBook = 
+                this.bookService.AddBook(inputBook);
+
+            // then
+            actualBook.Should().BeEquivalentTo(expectedBook);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertBook(inputBook),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- SallyLibrary.App/Program.cs
+++ SallyLibrary.App/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using SallyLibrary.App.Brokers.Storages;
+using SallyLibrary.App.Models.Books;
+using SallyLibrary.App.Services.Foundations.Books;
+
+var storageBroker = new StorageBroker();
+var bookService = new BookService(storageBroker);
+var book = new Book();
+
+bookService.AddBook(book);
