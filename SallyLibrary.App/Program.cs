c925b2a0e2ade845ac9187b2db9c1e2f2819a93a
Hassan Habib <harezk@microsoft.com>
2021-10-04 20:35:09 -0700

    CODE RUB: Update Libraries

--- SallyLibrary.App/Program.cs
+++ SallyLibrary.App/Program.cs
@@ -1,21 +1,14 @@
-﻿using SallyLibrary.App.Brokers.Storages;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using SallyLibrary.App.Brokers.Storages;
 using SallyLibrary.App.Models.Books;
 using SallyLibrary.App.Services.Foundations.Books;
 
 var storageBroker = new StorageBroker();
 var bookService = new BookService(storageBroker);
 var book = new Book();
-
 bookService.AddBook(null);
 bookService.ModifyBook(null);
-
-// measure the commitment between the clients, the services and the dependencies
-// based on the causes of the exceptions and it's ratios in your system
-
-// 100 requests 
-// 10 exceptions occurred
-// data is bad (OK)         90!! we need to talk to our customers.
-// service is bad (NOT OK)
-// dependency is bad (OK)
-
-// service - nobody knows how to use it?
