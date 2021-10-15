0206795a12d564ae16fe1513cd8cf01b37323dcd
Hassan Habib <harezk@microsoft.com>
2021-10-14 21:00:36 -0700

    BROKERS: Logging Broker

---
+++ SallyLibrary.App/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace SallyLibrary.App.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger logger;
+
+        public LoggingBroker(ILogger logger) =>
+            this.logger = logger;
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception.Message, exception);
+
+        public void LogDebug(string message) =>
+            this.logger.LogDebug(message);
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception.Message, exception);
+
+        public void LogInformation(string message) =>
+            this.logger.LogInformation(message);
+
+        public void LogTrace(string message) =>
+            this.logger.LogTrace(message);
+
+        public void LogWarning(string message) =>
+            this.logger.LogTrace(message);
+    }
+}
